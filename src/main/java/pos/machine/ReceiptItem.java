aa49a0755390e6c6f1b0bf98e09c846e2c479dc3
Cheuky Li <lich25@oocl.com>
2025-09-03 21:58:20 +0800

    add receiptitem class

---
+++ src/main/java/pos/machine/ReceiptItem.java
@@ -0,0 +1,31 @@
+package pos.machine;
+
+public class ReceiptItem {
+    private final String name;
+    private int quantity;
+    private final int unitPrice;
+    private int subTotal;
+
+    public ReceiptItem(String name, int quantity, int unitPrice) {
+        this.name = name;
+        this.quantity = 1;
+        this.unitPrice = unitPrice;
+        this.subTotal = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setQuantity(int quantity) {this.quantity = quantity;}
+
+    public int getQuantity() {return quantity;}
+
+    public int getPrice() {
+        return unitPrice;
+    }
+
+    public void setSubTotal(int subTotal) {this.subTotal = subTotal;}
+
+    public int getSubTotal() {return subTotal;}
+}
